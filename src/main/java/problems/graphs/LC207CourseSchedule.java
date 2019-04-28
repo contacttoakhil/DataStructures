45867c8dca27e2cdb650308759837abd90640644
Akhil Mittal <akhilmittal@Akhils-MacBook-Pro.local>
2019-04-28 21:03:15 +0530

    fixed adjacency map

--- src/main/java/problems/graphs/LC207CourseSchedule.java
+++ src/main/java/problems/graphs/LC207CourseSchedule.java
@@ -29,16 +29,16 @@ public class LC207CourseSchedule {
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] indegree = new int[numCourses];
-        Queue<Integer> queue = new LinkedList<Integer>();
-        Map<Integer, ArrayList<Integer>> map = initializeMap(numCourses, prerequisites, indegree);
+        Map<Integer, ArrayList<Integer>> adjacencyMap = initializeMap(numCourses, prerequisites, indegree);
         int count = numCourses;
+        Queue<Integer> queue = new LinkedList<Integer>();
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0)
+            if (indegree[i] == 0)  // travel backwards as we have pre-requisite relationship not adjacent with us
                 queue.offer(i);
         }
         while (!queue.isEmpty()) {
             int current = queue.poll();
-            for (int i : map.get(current)) {
+            for (int i : adjacencyMap.get(current)) {
                 if (--indegree[i] == 0)
                     queue.offer(i);
             }
@@ -46,15 +46,23 @@ public class LC207CourseSchedule {
         }
         return count == 0;
     }
+
     private Map<Integer,ArrayList<Integer>> initializeMap(int numCourses, int[][] prerequisites, int[] indegree) {
-        Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+        Map<Integer, ArrayList<Integer>> adjacencyMap = new HashMap<Integer, ArrayList<Integer>>(); //for given key provides all adjacent elements.
         for (int i = 0; i < numCourses; i++) {
-            map.put(i, new ArrayList<Integer>());
+            adjacencyMap.put(i, new ArrayList<Integer>());
         }
         for (int i = 0; i < prerequisites.length; i++) {
-            map.get(prerequisites[i][0]).add(prerequisites[i][1]);
-            indegree[prerequisites[i][1]]++;
+            int from = prerequisites[i][1];
+            int to = prerequisites[i][0];
+            adjacencyMap.get(from).add(to);
+            indegree[to]++;
         }
-        return map;
+        return adjacencyMap;
+    }
+
+    public static void main(String[] args) {
+        LC207CourseSchedule courseSchedule = new LC207CourseSchedule();
+        System.out.println(courseSchedule.canFinish(5, new int[][]{{0,1},{1,2},{1,3},{3,4}}));
     }
 }
